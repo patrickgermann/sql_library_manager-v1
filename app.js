5ebb2097787c4e7b66845fa52c76ab41e86ad248
Patrick Germann <p.germann@me.com>
2019-12-15 15:15:26 +0100

    fixed global error handler

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ app.use('/books', books);
 
 // catch 404 and forward to error handler
 app.use( (req, res, next) => {
-  next(createError(404));
+  const error = new Error("Sorry, no page with this name.");
+  error.status = 404;
+  next(error);
 });
 
 // error handler
@@ -28,9 +30,11 @@ app.use( (err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  console.log(err.status);
+  if (err.status === 404) {
+    res.render('page-not-found');
+  } else {
+    res.render('error');
+  }
 });
 module.exports = app;
--- routes/books.js
+++ routes/books.js
@@ -2,13 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Book = require('../models').Book;
 
-/* Handler function to wrap each route. */
 function asyncHandler(cb){
-  return async(req, res, next) => {
+  return async (req,res,next)=> {
     try {
-      await cb(req, res, next)
+      await cb(req,res,next);
     } catch(error){
-      res.status(500).send(error);
+      res.render('error', {error:err});
     }
   }
 }
